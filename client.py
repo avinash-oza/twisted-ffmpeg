9a11fe8051614ed6da728bd284b13609d69dcdee
Avinash Oza <avioza@gmail.com>
2014-03-03 10:55:36 -0500

    Actually commiting files from last commit

--- client.py
+++ client.py
@@ -3,25 +3,45 @@ from twisted.web.client import Agent,getPage
 from twisted.web.http_headers import Headers
 import urllib
 import logging
+import yaml
+import random
+import time
 
 logging.basicConfig(format= '%(asctime)s' + " Client: " +  '%(message)s', level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-def getNewJobs():
-    d = defer.Deferred()
-    reactor.callLater(2, d.callback, None)
-    getPage("http://127.0.0.1:8888/job_handler", method='POST', postdata=urllib.urlencode({'the-field':'Script_test'}),
-    headers = {'Content-Type':'application/x-www-form-urlencoded'})
-    return d
+class Client(object):
+    def getNewJobs(self):
+    #   d = defer.Deferred()
+        self.d = getPage("http://127.0.0.1:8888/job_handler")
+        reactor.callLater(2, self.processJob, None)
+        return self.d
 
-def printLog(d):
-    log.info("Got the callback")
+    def sleep(self, secs):
+        d = defer.Deferred()
+        reactor.callLater(secs, d.callback, None)
+        return d
 
-def cbStop(d):
-    reactor.stop()
+    def processJob(self, result):
+        d = self.d
+        self.d = None
+        if result is None:
+            #TODO : Add logic here to retry after a random interval
+    #       retry = random.randint(30, 60)
+            retry = 10
+            log.info("No jobs available. Trying again in " + str(retry) + " seconds." )
+            time.sleep(retry)
+            log.info("Slept")
+            self.d = self.getNewJobs()
+        else:
+            # Got a job, time to process it
+            file_name = yaml.load(result)[0]
+            log.info("Got file " + file_name)
 
-d = getNewJobs()
-d.addCallback(printLog)
-d.addCallback(cbStop)
+client = Client()        
+d = client.getNewJobs()
+d.addCallback(client.processJob)
 
+#   getPage("http://127.0.0.1:8888/job_handler", method='POST', postdata=urllib.urlencode({'the-field':'Script_test'}),
+#   headers = {'Content-Type':'application/x-www-form-urlencoded'})
 reactor.run()
--- job_server.py
+++ job_server.py
@@ -1,9 +1,11 @@
+import Queue
 from twisted.web.server import Site
 from twisted.web.resource import Resource
 from twisted.web.static import File
 from twisted.internet import reactor
 
 import cgi
+import yaml
 import logging
 logging.basicConfig(format= '%(asctime)s' + " Server: " +  '%(message)s', level=logging.DEBUG)
 log = logging.getLogger(__name__)
@@ -11,11 +13,33 @@ log = logging.getLogger(__name__)
 class JobProvider (Resource):
     def __init__(self):
         Resource.__init__(self)
+        self.available_jobs = Queue.Queue()
+        
+        log.info("Loading pending jobs from file")
+        jobs = self.get_pending_jobs()
+        for job in jobs:
+            self.available_jobs.put(job)
+        log.info("Finished adding " + str(len(jobs)) + " pending video files")
+    def get_pending_jobs(self):
+        """
+        Gives a new file name for the client to download and convert
+        """
+        jobs = set()
+        f = open("data.yaml", 'r')
+        jobs = yaml.load(f.read())
+        return jobs
 
     def render_GET(self, request):
-        logging.info("Job Provider Called")
-        return "This is the job provider" + '<html><body><form method="POST"><input name="the-field" type="text" /></form></body></html>'
-
+        try:
+            file_path = self.available_jobs.get(block=False)
+            log.info("TBD HOST:" + "Provided file " + file_path + "as encoding task") 
+            
+            return yaml.dump([file_path])
+#           return "This is the job provider" + '<html><body><form method="POST"><input name="the-field" type="text" /></form></body></html>'
+        except Queue.Empty,e:
+            file_path = None
+            log.info("TBD_HOST:" +" NO FILES AVAILABLE TO ENCODE") 
+            return yaml.dump([])
     def render_POST(self, request):
         logging.info("Got " + str(request.args['the-field']))
         return '<html><body>You submitted: %s</body></html>' % (cgi.escape(request.args["the-field"][0]),)
