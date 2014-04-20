0939856446942fcb3069d66304837990014f142e
Avinash Oza <avioza@gmail.com>
2014-04-20 01:48:23 -0400

    Test commit with keys

--- job_generator.py
+++ job_generator.py
@@ -1,9 +1,12 @@
 import os
 import sys
+from celery_task import encode_video
 
 root = sys.argv[1]
 for root, dirs, files in os.walk(root):
     if len(files):
-        for file in files:
-           if file.endswith('.avi'):
-                print("Adding {0} to list" .format(os.path.join(root,file)))
+        for f in files:
+           if f.endswith('.avi'):
+                the_file = os.path.join(root, f)
+                print("Adding {0} to list" .format(the_file))
+                encode_video(the_file, f + "-out")
