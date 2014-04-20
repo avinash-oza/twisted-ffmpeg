f7d697eb8972a4892111b59dfe47d5be8836ab87
Avinash Oza <avioza@gmail.com>
2014-04-20 19:05:12 -0400

    Added basic job controller

--- job_generator.py
+++ job_generator.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import time
 from celery_task import encode_video
 
 root = sys.argv[1]
+num_jobs = 0
 for root, dirs, files in os.walk(root):
     if len(files):
         for f in files:
@@ -10,5 +12,9 @@ for root, dirs, files in os.walk(root):
                 the_file = os.path.join(root, f)
                 the_path = os.path.splitdrive((the_file))
                 final_path = the_path[1].replace("\\", "/")
+                num_jobs += 1
+                if num_jobs % 10 == 0:
+                    print "Sleeping for 5 minutes"
+                    time.sleep(300)
                 print("Adding {0} to list" .format(final_path))
-                encode_video(final_path, "OUTPUT_" + f)
+                encode_video.delay(final_path, "OUTPUT_" + f)
