650c0bf9edea14afc659d03f1b60f8c8129c9310
Avinash Oza <avioza@gmail.com>
2016-04-21 22:06:29 -0400

    Update FileDescription tuple to contain the source path if it exists, update tuple to not be verbose Update abstractFileTransferAdapter to actually be abstract

--- file_transfer_adapters/abstract_file_transfer_adapter.py
+++ file_transfer_adapters/abstract_file_transfer_adapter.py
@@ -1,14 +1,17 @@
 """
 Defines the basic interface an adapter for a protocol should implement
 """
+from abc import abstractmethod
 from collections import namedtuple
-FileDescription = namedtuple('FileDescription', 'file_name', 'channel', 'time_period_start', 'time_period_end', 'ip_address')
+from file_description import FileDescription
 
 class AbstractFileTransferAdapter(object):
-    def get_file(self, file_destination, file_description=None, file_path=None):
-        """TBD: Given a FileDescription or file_path, this retrieves the file and gives a return code"""
-            raise NotImplementedError
+    @abstractmethod
+    def get_file(self, file_description):
+        """TBD: Given a FileDescription, this figures out which file to obtain"""
+        pass
 
-    def put_file(self, full_source_path, file_description):
-        """TBD: Given the full_source_path, this will transfer the file using the description of the file"""
-            raise NotImplementedError
+    @abstractmethod
+    def put_file(self, file_description):
+        """TBD: Give the file description and the original source path, this method will transfer the file remotely"""
+        pass
