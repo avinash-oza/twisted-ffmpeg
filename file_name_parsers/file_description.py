b2dbb4e13b9c04ba783fabe532150d07663db9c8
Avinash Oza <avioza@gmail.com>
2016-05-17 20:34:26 -0400

    Change FileDescription to have a field of output_file_name and have a default initalizer

--- file_name_parsers/file_description.py
+++ file_name_parsers/file_description.py
@@ -1,2 +1,3 @@
 from collections import namedtuple
-FileDescription = namedtuple('FileDescription', ['file_name', 'channel', 'time_period_start', 'time_period_end', 'ip_address', 'segment_date', 'full_source_path'], verbose=False)
+FileDescription = namedtuple('FileDescription', ['file_name', 'channel', 'time_period_start', 'time_period_end', 'ip_address', 'segment_date', 'full_source_path', 'output_file_name'], verbose=False)
+FileDescription.__new__.__defaults__ = (None,) * len(FileDescription._fields)
