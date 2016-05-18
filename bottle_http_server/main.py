9829f453b646329d75902f549f7f10866fde6f00
Avinash Oza <avioza@gmail.com>
2016-05-18 12:51:55 -0400

    Add in pyyaml as requirement. Implement file_list function

--- bottle_http_server/main.py
+++ bottle_http_server/main.py
@@ -1,10 +1,37 @@
-from bottle import Bottle, run
+"""
+This is a simple standalone http server that supports getting a file and deleting a file. There is no authentication or any protection so use wisely!
+"""
+import os
+import sys
+import logging
+import yaml
+from bottle import Bottle, run, request
+from application_config import ApplicationConfig
+logging.basicConfig(format= '%(asctime)s ' + '%(message)s', level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 app = Bottle()
 
-@app.route('/hello')
-def hello():
-    return 'Hello World!'
+@app.route('/get_file_list')
+def get_file_list():
+    config = ApplicationConfig('bottle_http_server.cfg')
+    file_root = config.get_config_option('Default', 'root_directory')
+    log.info("file_root is {0}".format(file_root))
+#   addressing_scheme, network_location, path, query, fragment_identifier = request.urlparts
+#   log.info("Request url is {0}".format(network_location))
+
+    final_file_paths = []
+    for dir_path, _, filenames in os.walk(file_root):
+        for filename in filenames:
+            #TODO: Make this configurable
+            if filename.endswith('.avi'):
+                final_file_paths.append(os.path.join(dir_path, filename))
+
+    final_results = {'number_of_entries' : len(final_file_paths),
+                     'file_paths' : final_file_paths
+                    }
+    return yaml.dump(final_results)
+
 
 
 run(app, host='localhost', port=8080)
